611080efe708bacd2009ace27f084dc392d34e3c
dansj56 <31407625+dansj56@users.noreply.github.com>
2026-03-14 15:09:22 +0100

    Initial

---
+++ Assignment2.py
@@ -0,0 +1,12 @@
+#%%
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# %%
+#%%
+trail1 = pd.read_csv('Trail1_extracted_features_acceleration_m1ai1-1-1.csv')
+print(trail1)
+print(trail1.shape)
+
+#%% 
