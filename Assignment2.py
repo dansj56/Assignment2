47bb4e2bb2f3a8d8f75dea6d67839c8695bf692b
dansj56 <31407625+dansj56@users.noreply.github.com>
2026-03-15 14:28:33 +0100

    Added K-fold, stratified split, K-fold on entire set.

--- Assignment2.py
+++ Assignment2.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 
@@ -35,7 +35,9 @@ trail = traildata.drop(columns='event')
 
 # %%
 #Splitting into training and testing data sets
-trail_train, trail_test, target_train, target_test = train_test_split(trail, trailtarget, test_size=0.2, random_state=42)
+trail_train, trail_test, target_train, target_test = (
+    train_test_split(trail, trailtarget, test_size=0.2, random_state=42, stratify=trailtarget)
+)
 
 # %%
 #Normalize features
@@ -45,8 +47,9 @@ trail_train_scaled = pd.DataFrame(
     scaler.fit_transform(trail_train),
     columns=trail_train.columns
 )
+#Scale the test set based on the scaling from the learning.
 trail_test_scaled = pd.DataFrame(
-    scaler.fit_transform(trail_test),
+    scaler.transform(trail_test),
     columns=trail_test.columns
 )
 #print(f'Before scaling - Mean: {trail.mean().mean():.4f}, Std: {trail.std().mean():.4f}')
@@ -54,7 +57,7 @@ trail_test_scaled = pd.DataFrame(
 
 #%%
 #Train SVM-model
-model = SVC(gamma='auto')
+model = SVC(gamma='auto', random_state=42)
 model.fit(trail_train_scaled,target_train)
 
 # %%
@@ -64,6 +67,22 @@ predictions = model.predict(trail_test_scaled)
 #%%
 #Evaluate
 print(f'Classification report: \n {classification_report(target_test, predictions, target_names=['Normal', 'Anomaly'])}')
-print(f'Accuracy score: {accuracy_score(target_test, predictions)}')
+print(f'Accuracy score: {accuracy_score(target_test, predictions):.5f}')
 
 # %%
+#Using KFold cross validation on the test set only.
+#kf = KFold(n_splits=5)
+scores = cross_val_score(model, trail_train_scaled, target_train, cv=5)
+#print(f'Scores from KFold:  {scores}')
+print(f'Mean of scores: {scores.mean():.5f}')
+print(f'Standard deviation of scores: {scores.std():.5f}')
+
+# %%
+#Using KFold cross validation on the ENTIRE set.
+scaler_full = StandardScaler()
+trail_scaled_full = scaler_full.fit_transform(trail)
+scores_full = cross_val_score(model, trail_scaled_full, trailtarget, cv=5)
+print(f"K-Fold Entire dataset Mean:  {scores_full.mean():.5f}")
+print(f"K-Fold Std Deviation:   {scores_full.std():.5f}")
+
+#%%
